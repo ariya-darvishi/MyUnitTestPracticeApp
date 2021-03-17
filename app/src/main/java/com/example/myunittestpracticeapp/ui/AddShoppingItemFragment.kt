5ef752b3638d95ac735ab6483f4c6ada0dadad6f
Amirreza.dq <amirreza.dq@gmail.com>
2021-03-17 11:03:31 +0330

    Setup ui :)

---
+++ app/src/main/java/com/example/myunittestpracticeapp/ui/AddShoppingItemFragment.kt
@@ -0,0 +1,14 @@
+package com.example.myunittestpracticeapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.myunittestpracticeapp.R
+
+class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/example/myunittestpracticeapp/ui/ShoppingFragment.kt
@@ -0,0 +1,17 @@
+package com.example.myunittestpracticeapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myunittestpracticeapp.R
+
+class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
