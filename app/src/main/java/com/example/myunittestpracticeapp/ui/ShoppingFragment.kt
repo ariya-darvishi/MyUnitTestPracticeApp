99d43a6ebb622ebcb2497080f001d2b63dfdab59
Amirreza.dq <amirreza.dq@gmail.com>
2021-03-23 11:51:53 +0430

    Setup Swipe to Delete Functionality in ShoppingFragment.kt :)

--- app/src/main/java/com/example/myunittestpracticeapp/ui/ShoppingFragment.kt
+++ app/src/main/java/com/example/myunittestpracticeapp/ui/ShoppingFragment.kt
@@ -5,26 +5,84 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.LEFT
+import androidx.recyclerview.widget.ItemTouchHelper.RIGHT
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myunittestpracticeapp.R
+import com.example.myunittestpracticeapp.adapters.ShoppingItemAdapter
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_shopping.*
+import javax.inject.Inject
 
-class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
+class ShoppingFragment @Inject constructor(
+    val shoppingItemAdapter: ShoppingItemAdapter,
+    var viewModel: ShoppingViewModel? = null
+) : Fragment(R.layout.fragment_shopping) {
 
-    lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        viewModel =
+            viewModel ?: ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        subscribeToObservers()
+        setupRecyclerView()
 
         fabAddShoppingItem.setOnClickListener {
             findNavController().navigate(
                 ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
             )
         }
+    }
 
+
+    private val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+        0, LEFT or RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ) = true
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+            val itemPosition = viewHolder.layoutPosition
+            val item = shoppingItemAdapter.shoppingItems[itemPosition]
+
+            viewModel?.deleteShoppingItem(item)
+
+            Snackbar.make(requireView(), "Successfully deleted item", Snackbar.LENGTH_LONG).apply {
+                setAction("Undo") {
+                    viewModel?.insertShoppingItemIntoDb(item)
+                }
+                show()
+            }
+        }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel?.shoppingItem?.observe(viewLifecycleOwner, Observer {
+            shoppingItemAdapter.shoppingItems = it
+        })
+        viewModel?.totalPrice?.observe(viewLifecycleOwner, Observer {
+            val price = it ?: 0f
+            val priceText = "Total Price: $price$"
+            tvShoppingItemPrice.text = priceText
+        })
+    }
+
+    private fun setupRecyclerView() {
+        rvShoppingItems.apply {
+            adapter = shoppingItemAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            ItemTouchHelper(itemTouchCallback).attachToRecyclerView(this)
+        }
     }
 
 }
