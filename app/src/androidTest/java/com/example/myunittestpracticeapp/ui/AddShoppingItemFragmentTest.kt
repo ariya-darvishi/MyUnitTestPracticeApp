54ed41e0c006aa14de9e9da7dc3d4b6bf66cc97c
Amirreza.dq <amirreza.dq@gmail.com>
2021-03-23 10:31:13 +0430

    Tested Addition of Shopping Items :)

--- app/src/androidTest/java/com/example/myunittestpracticeapp/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/example/myunittestpracticeapp/ui/AddShoppingItemFragmentTest.kt
@@ -1,12 +1,20 @@
 package com.example.myunittestpracticeapp.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.example.myunittestpracticeapp.R
+import com.example.myunittestpracticeapp.data.local.ShoppingItem
+import com.example.myunittestpracticeapp.getOrAwaitValue
 import com.example.myunittestpracticeapp.launchFragmentInHiltContainer
+import com.example.myunittestpracticeapp.repositories.FakeShoppingRepositoryAndroidTest
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -15,28 +23,57 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
-class AddShoppingItemFragmentTest{
+class AddShoppingItemFragmentTest {
 
     @get:Rule
-    var hiltRule= HiltAndroidRule(this)
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
 
 
     @Before
-    fun setup(){
+    fun setup() {
         hiltRule.inject()
     }
 
+    @Test
+    fun clickInsertIntoDn_shoppingItemInsertedIntoDb() {
+
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+
+            viewModel = testViewModel
+
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("shopping item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("6"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("6.4"))
+
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItem.getOrAwaitValue()).
+                contains(ShoppingItem("shopping item", 6, 6.4f, ""))
+
+    }
 
     @Test
     fun pressBackButton_popBackStack() {
 
         val navController = mock(NavController::class.java)
 
-        launchFragmentInHiltContainer<AddShoppingItemFragment>{
+        launchFragmentInHiltContainer<AddShoppingItemFragment> {
             Navigation.setViewNavController(requireView(), navController)
         }
 
