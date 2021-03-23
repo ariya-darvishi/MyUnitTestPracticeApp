0d04f4572f1e6865b675e937bac4f4d4dfa38a7f
Amirreza.dq <amirreza.dq@gmail.com>
2021-03-23 10:13:39 +0430

    Setup AddShoppingItemFragment :)

--- app/src/main/java/com/example/myunittestpracticeapp/ui/AddShoppingItemFragment.kt
+++ app/src/main/java/com/example/myunittestpracticeapp/ui/AddShoppingItemFragment.kt
@@ -5,12 +5,22 @@ import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
 import com.example.myunittestpracticeapp.R
+import com.example.myunittestpracticeapp.util.Status
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
+import kotlinx.android.synthetic.main.fragment_shopping.*
 import kotlinx.android.synthetic.main.item_image.*
+import kotlinx.android.synthetic.main.item_image.ivShoppingImage
+import javax.inject.Inject
 
-class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+class AddShoppingItemFragment @Inject constructor(
+    val glide: RequestManager
+) : Fragment(R.layout.fragment_add_shopping_item) {
 
     lateinit var viewModel: ShoppingViewModel
 
@@ -19,13 +29,23 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
 
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
 
+        subscribeToObservers()
+
+        btnAddShoppingItem.setOnClickListener {
+            viewModel.insertShoppingItem(
+                etShoppingItemName.text.toString(),
+                etShoppingItemAmount.text.toString(),
+                etShoppingItemPrice.text.toString()
+            )
+        }
+
         ivShoppingImage.setOnClickListener {
             findNavController().navigate(
                 AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
             )
         }
 
-        val callback = object : OnBackPressedCallback(true){
+        val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 viewModel.setCurrentImageUrl("")
                 findNavController().popBackStack()
@@ -34,6 +54,41 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
 
         requireActivity().onBackPressedDispatcher.addCallback(callback)
 
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.currentImageUrl.observe(viewLifecycleOwner, Observer {
+            glide.load(it).into(ivShoppingImage)
+        })
+
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            "Added Shopping Item",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        findNavController().popBackStack()
+                    }
+
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            result.message ?: "An unknown error occurred",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        /* NO-OP */
+                    }
+
+                    Status.LOADING -> {
+                    }
+                }
+            }
+        })
 
     }
+
+
 }
--- app/src/main/java/com/example/myunittestpracticeapp/ui/ShoppingFragmentFactory.kt
+++ app/src/main/java/com/example/myunittestpracticeapp/ui/ShoppingFragmentFactory.kt
@@ -2,17 +2,20 @@ package com.example.myunittestpracticeapp.ui
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import com.bumptech.glide.RequestManager
 import com.example.myunittestpracticeapp.adapters.ImageAdapter
 import javax.inject.Inject
 
 class ShoppingFragmentFactory @Inject constructor(
-    private val imageAdapter: ImageAdapter
+    private val imageAdapter: ImageAdapter,
+    private val glide: RequestManager
 ) : FragmentFactory() {
 
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
 
         return when (className) {
             ImagePickFragment::class.java.name -> ImagePickFragment(imageAdapter)
+            AddShoppingItemFragment::class.java.name -> AddShoppingItemFragment(glide)
             else -> super.instantiate(classLoader, className)
         }
 
